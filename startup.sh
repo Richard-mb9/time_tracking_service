f9f2f4c2ffea4ac07b9b4e84d9e1136432dac5da
Richard <richard_mbs@hotmail.com>
2026-02-28 12:39:42 -0300

    ajuste no startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,7 @@ if [ $# -ne 0 ]; then
 else
     chmod +x ./update_database.sh
     ./update_database.sh
-    exec uvicorn main:app --host 0.0.0.0 --port 8083 --app-dir src --workers 2
+    
+    WORKERS="${UVICORN_WORKERS:-1}"
+    exec uvicorn main:app --host 0.0.0.0 --port 8083 --app-dir src --workers $WORKERS
 fi
